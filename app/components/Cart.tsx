87f57f4d85d4610a5634573c9ec8e09cd22aca7a
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-15 21:39:07 -0600

    updating cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -7,10 +7,11 @@ import formatPrice from "@/util/PriceFormat"
 
 export default function Cart(){
     const cartStore = useCartStore()
+    
     console.log(cartStore.isOpen)
     return(
+        
         <div className="fixed w-full h-screen left-0 top-0 bg-black/25">
-            
             
             <div  onClick={() => cartStore.toggleCart()} 
                   className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700 "
